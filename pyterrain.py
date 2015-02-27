8a4c38a234da617994a81bef7e6a41c49beb0478
Zachary Stewart <zstewar1@umbc.edu>
2015-02-27 18:53:40 -0500

    added ability to swap the colors of things

--- pyterrain.py
+++ pyterrain.py
@@ -6,16 +6,12 @@ import sys
 import noise
 import pygame
 
-SEA_COLOR = (0,0,255)
-COAST_COLOR = (0,100,255)
-SHORE_COLOR = (244,164,96)
-LAND_COLOR = (183,123,72)
-MOUNTAIN_COLOR = (122,102,78)
-
-def main(width, height, sea_level, xoffset=0, yoffset=0, outfile='testmap.png'):
+def main(width=1024, height=1024, sea_level=5000, xoffset=0, yoffset=0, outfile='testmap.png',
+         sea_color=(0,0,255), coast_color=(0,100,255), shore_color=(244,164,96), land_color=(183,123,72),
+         mountain_color=(122,102,78)):
 
   worldmap = pygame.Surface((width, height))
-  worldmap.fill(SEA_COLOR)
+  worldmap.fill(sea_color)
 
   freq_mod = 3/float(width)
 
@@ -27,27 +23,43 @@ def main(width, height, sea_level, xoffset=0, yoffset=0, outfile='testmap.png'):
       if land_level > sea_level:
         if land_level < 5000:
           if (land_level - sea_level) < 1000:
-            worldmap.set_at((x, y), SHORE_COLOR)
+            worldmap.set_at((x, y), shore_color)
           else:
-            worldmap.set_at((x, y), LAND_COLOR)
+            worldmap.set_at((x, y), land_color)
         else:
-          worldmap.set_at((x, y), MOUNTAIN_COLOR)
+          worldmap.set_at((x, y), mountain_color)
       elif (sea_level - land_level) < 1000:
-        worldmap.set_at((x, y), COAST_COLOR)
+        worldmap.set_at((x, y), coast_color)
 
   pygame.image.save(worldmap, outfile)
- 
+
+def color(s):
+  if s.startswith('#'):
+    ss = s[1:]
+    if len(ss) == 3:
+      return tuple(int(c * 2, base=16) for c in ss)
+    elif len(ss) == 6:
+      return tuple(int(ss[i:i+2], base=16) for i in range(0, len(ss), 2))
+    else:
+      raise ValueError('Invalid literal "{}" for hexidecimal color'.format(s))
+  else:
+    r,g,b = tuple(min(max(int(i), 0), 255) for i in s.replace(':', ',').split(','))
+    return r, g, b
+  
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('width', type=int, help='Width of the final image')
   parser.add_argument('height', type=int, help='Height of the final image')
   parser.add_argument('sea_level', type=float, help="How high should the map's sea level be")
-  parser.add_argument('-x', '--xoffset', type=float,
-                      help='Offset to apply to the horizontal noise position', default=0)
-  parser.add_argument('-y', '--yoffset', type=float,
-                      help='Offset to apply to the vertical noise position', default=0)
-  parser.add_argument('-o', '--outfile', type=str, help='File to write the map image to',
-                      default='testmap.png')
-  args = parser.parse_args()
-  print(args)
-  main(**vars(args))
+  parser.add_argument('-x', '--xoffset', type=float, help='Offset to apply to the horizontal noise position')
+  parser.add_argument('-y', '--yoffset', type=float, help='Offset to apply to the vertical noise position')
+  parser.add_argument('-o', '--outfile', type=str, help='File to write the map image to')
+
+  parser.add_argument('--sea_color', type=color, help='Color for deep water')
+  parser.add_argument('--coast_color', type=color, help='Color for water near land')
+  parser.add_argument('--shore_color', type=color, help='Color for land near water')
+  parser.add_argument('--land_color', type=color, help='Color for land')
+  parser.add_argument('--mountain_color', type=color, help='Color for mountains')
+  
+  args = {k:v for k,v in vars(parser.parse_args()).items() if v is not None}
+  main(args)
